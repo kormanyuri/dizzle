5f8ab8e5c39430dbf18993449459c3a0d43d81ef
korman <korman.yuri@gmail.com>
2017-11-13 16:32:01 +0200

    add Auth

---
+++ src/components/Auth.js
@@ -0,0 +1,16 @@
+/**
+ * Created by korman on 02.10.17.
+ */
+
+export default class Auth
+{
+    checkAuth(){
+        const token = window.localStorage.getItem('token');
+
+        if (token) {
+            return true;
+        } else {
+            window.location = '/#/login';
+        }
+    }
+}
