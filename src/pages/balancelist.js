400dc87f0c80066615ec744a054291fe9dffa686
korman <korman.yuri@gmail.com>
2017-11-14 14:58:34 +0200

    move my account

--- src/pages/balancelist.js
+++ src/pages/balancelist.js
@@ -13,6 +13,10 @@ import Avatar1 from '../img/avatar-1.jpg';
 import Avatar2 from '../img/avatar-2.jpg';
 import Avatar3 from '../img/avatar-3.jpg';
 
+import Auth from '../components/Auth';
+import Config from '../Config';
+import axios from 'axios';
+
 let width=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
 let scrollHeight = Math.max(
@@ -36,44 +40,84 @@ const styles = theme =>  ({
     },
 });
 
-class GroupBuyList extends Component {
-    state = {
-        open: false,
-    };
+class BalanceList extends Component {
+
+    constructor(props){
+        super(props);
+        const config = new Config();
+
+        this.state = {
+            items: [],
+            baseUrl: config.baseUrl,
+            message: '',
+            open: false
+        };
+    }
+
+    componentWillMount(){
+        const token = window.localStorage.getItem('token');
+
+        this.setState({
+            message: 'Load...'
+        });
+
+        axios.get(this.state.baseUrl + 'gift-card/rest/shopper-balance/0', {
+            params: {
+                token: window.localStorage.getItem('token'),
+                method: 'LIST'
+            }
+        })
+            .then(response => {
+                console.log(response);
+
+                if (typeof response.data.message == 'undefined') {
+                    this.setState({
+                        items: response.data
+                    });
+                } else {
+                    this.setState({
+                        message: 'List empty'
+                    });
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
     render() {
 
-        return (
-            <div className={this.props.classes.root}>
-                <MyAppBar title="Balanse"/>
-                <MyCardBalance
-                    name="Starbucks"
-                    avatar={Avatar1}
-                    giftcard="$100"
-                    groupbuyowner="jacky"
-                    href="#ingroupbuy"
-                />
-                <MyCardBalance
-                    name="subway"
-                    avatar={Avatar2}
-                    giftcard="$100"
-                    groupbuyowner="yourself"
-                    href="#ingroupbuy"
-                />
-                <MyCardBalance
-                    name="mccafe"
-                    avatar={Avatar3}
-                    giftcard="$100"
-                    groupbuyowner="Mike"
-                    href="#ingroupbuy"
-                />
-            </div>
-        );
+        if (this.state.items.length > 0) {
+            return (
+                <div className={this.props.classes.root}>
+                    <MyAppBar title="Balanse"/>
+                    {
+                        this.state.items.map((item, key) =>
+                            <MyCardBalance
+                                key={key}
+                                name={item.shopper.name}
+                                avatar={Avatar1}
+                                giftcard={`$` + item.balance / 100}
+                                groupbuyowner="jacky"
+                                href={`/#/balance-list/${item.shopper.id}`}
+                            />
+                        )
+                    }
+                </div>
+            );
+        } else {
+            return (
+                <div className={this.props.classes.root}>
+                    <MyAppBar title="Balance"/>
+                    {this.state.message}
+                </div>
+            );
+        }
     }
 }
 
-GroupBuyList.propTypes = {
+BalanceList.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withRoot(withStyles(styles)(GroupBuyList));
+export default withRoot(withStyles(styles)(BalanceList));
