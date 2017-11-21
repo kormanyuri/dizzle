bfdb38264660e0e4d95e97750efe8b0310ff451a
korman <korman.yuri@gmail.com>
2017-11-21 14:45:18 +0200

    changed indes

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import GroupBuyList from './pages/GroupBuyList';
-import InGroupBuy from './pages/InGroupBuy';
-import InGroupBuySuccessful from './pages/InGroupBuySuccessful';
-import BalanceList from './pages/BalanceList';
-import Transactions from './pages/Transactions';
-import MyAccount from './pages/MyAccount';
-import EditProfile from './pages/EditProfile';
-import QRqode from './pages/Qr';
-// import TestPageA from './pages/TestPageA';
-// import TestPageB from './pages/TestPageB';
+import Login from './pages/consumer/Login';
+import PluginLogin from './pages/plugin/Login';
+import SignUp from './pages/consumer/SignUp';
+import GroupBuyList from './pages/consumer/GroupBuyList';
+import InGroupBuy from './pages/consumer/InGroupBuy';
+import InGroupBuySuccessful from './pages/consumer/InGroupBuySuccessful';
+import BalanceList from './pages/consumer/BalanceList';
+import Transactions from './pages/consumer/Transactions';
+import MyAccount from './pages/consumer/MyAccount';
+import EditProfile from './pages/consumer/EditProfile';
+import QRqode from './pages/consumer/Qr';
+import TestPageA from './pages/consumer/TestPageA';
+import TestPageB from './pages/consumer/TestPageB';
+
+
+import GiftCardsList from './pages/plugin/GiftCardsList';
+import Payment from './pages/plugin/Payment';
+import OrderAccepted from './pages/plugin/OrderAccepted';
+import OrderAcceptedInviteFriends from './pages/plugin/OrderAcceptedInviteFriends';
+import InfGroup from './pages/plugin/InfGroup';
+import PaymentBuyNow from './pages/plugin/PaymentBuyNow';
+import PaymentBuyTogether from './pages/plugin/PaymentBuyTogether';
 
 import { HashRouter,Route} from 'react-router-dom';
 
@@ -39,6 +49,19 @@ export default class Index extends React.Component {
                         <Route exact path="/myaccount" component={MyAccount}/>
                         <Route exact path="/editprofile" component={EditProfile}/>
                         <Route exact path="/qr" component={QRqode}/>
+                        <Route exact path="/test-page-a" component={TestPageA}/>
+                        <Route exact path="/test-page-b" component={TestPageB}/>
+
+                        <Route exact path="/plugin/login" component={PluginLogin}/>
+                        <Route exact path="/plugin/login/:shopperId" component={PluginLogin}/>
+                        <Route exact path="/plugin/gift-cards-list/:shopperId" component={GiftCardsList}/>
+                        <Route exact path="/plugin/friend-buy/:groupBuyId" component={GiftCardsList}/>
+                        <Route exact path="/plugin/inf-group/:id" component={InfGroup}/>
+                        <Route exact path="/plugin/payment" component={Payment}/>
+                        <Route exact path="/plugin/payment-buy-now" component={PaymentBuyNow}/>
+                        <Route exact path="/plugin/payment-buy-together" component={PaymentBuyTogether}/>
+                        <Route exact path="/plugin/order-accepted" component={OrderAccepted}/>
+                        <Route exact path="/plugin/order-accepted-invite-friends" component={OrderAcceptedInviteFriends}/>
 
                     </div>
                 </HashRouter>
